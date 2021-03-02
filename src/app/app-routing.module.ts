ad187cce8353e6995dc7ef895d4e1f39b17cd958
sultanul alam <sultanulalam21@gmail.com>
2021-03-02 08:41:23 +0600

    initial stage complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {FeedbackComponent} from './feedback/feedback.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+ {path: 'feedback', component: FeedbackComponent}
 ];
 
 @NgModule({
