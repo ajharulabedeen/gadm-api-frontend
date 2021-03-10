64d3f28573a9be7803d77e92c28531a91b563c53
ajharulabedeen <ajharulabedeen@gmail.com>
2021-03-10 21:41:19 +0600

    css removed from the home component css!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+    './home.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
